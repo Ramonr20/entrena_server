92205efe2fa2068498adb16b0f322047ab83ad39
Ramonr20 <hermon.rios@outlook.com>
2020-03-28 12:56:36 -0600

    Deactivte permission_classes

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 
 class GetNodeView(generics.RetrieveAPIView):
     authentication_classes = [TokenAuthentication]    
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     serializer_class = RegNodeSerializer
     queryset = Reg_Node.objects.all()
@@ -25,7 +25,7 @@ class GetNodeView(generics.RetrieveAPIView):
 
 class SetNodeView(generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]    
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     queryset = Reg_Node.objects.all()
     serializer_class = RegNodeSerializer
